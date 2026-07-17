5b621150c2778d0642438ea209c430e0b26ba6a0
Juan Eulate <juan_a_eulate@hotmail.com>
2026-07-17 14:52:26 -0400

    feat: add reminder domain contracts

---
+++ src/features/reminders/index.ts
@@ -0,0 +1,11 @@
+export {
+  REMINDER_CHANNELS,
+  REMINDER_SOURCES,
+  REMINDER_STATUSES,
+  type Reminder,
+  type ReminderChannel,
+  type ReminderIdentityInput,
+  type ReminderSource,
+  type ReminderStatus,
+} from "./reminder.types";
+export { buildReminderId } from "./reminder-id";
